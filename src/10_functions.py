6b63dc3b20eeb6b4b0e96e5c1ea81bf2f9ee4635
CarnunMP <carnun@hotmail.co.uk>
2020-02-10 15:32:43 +0000

    10 done

--- src/10_functions.py
+++ src/10_functions.py
@@ -9,4 +9,7 @@ num = int(num)
 # Print out "Even!" if the number is even. Otherwise print "Odd"
 
 # YOUR CODE HERE
+def is_even(num):
+  return num % 2 == 0
 
+print("Even!") if is_even(num) else print("Odd!")
